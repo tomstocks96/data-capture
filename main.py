741ff659769b464232dd2c7a83970026e11a0945
Thomas Stocks <tstocks@mercedesamgf1.com>
2023-02-23 16:49:30 +0000

    finishing docker

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     url = 'https://livetiming.tsl-timing.com/230812?_gl=1*um61uw*_ga*MTA1MzIxMTgzOC4xNjc2NjQwMDk1*_ga_B9ZGX55TXH*MTY3NzEzODM5My4yLjEuMTY3NzE0MDgwMi4wLjAuMA..'
     parser = TableParser()
 
-    kafka_producer = Producer({'bootstrap.servers': 'localhost:29092'})
+    kafka_producer = Producer({'bootstrap.servers': 'kafka:29092'})
 
     with TslInterface(session_url=url) as tsl_interface:
         metadata_in_row = ['Name', 'No']
