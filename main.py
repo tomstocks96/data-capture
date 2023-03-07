fdf9cf4c6bea14d77411a5f486b95089fb2529ac
Thomas Stocks <tstocks@mercedesamgf1.com>
2023-03-07 15:27:15 +0000

    reducing sleep between scrapes

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ if __name__ == '__main__':
             kafka_producer.flush()
             
             logger.info('sleeping before next scrape')
-            time.sleep(30)
+            time.sleep(8)
 
