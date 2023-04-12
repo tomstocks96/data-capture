193a25b4725bf87d49aa40205e13e345a83063b5
Thomas Stocks <tstocks@mercedesamgf1.com>
2023-04-12 10:41:03 +0100

    adding support for arguments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, time, datetime, logging, json, sys, os
+import requests, time, datetime, logging, json, sys, os, argparse
 
 from confluent_kafka import Producer
 
@@ -6,6 +6,23 @@ from tsl_interface.tsl_interface import TslInterface
 from table_parser.table_parser import TableParser
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--environment', default='docker')
+    
+    args = parser.parse_args()
+    
+    if args.environment == 'host':
+        KAFKA_URL = 'localhost:9092'
+        AGENT_URL = 'http://localhost:4444/wd/hub'
+        SCRAPE_URL = 'https://livetiming.tsl-timing.com/231541'
+        
+    elif args.environment == 'docker':
+        KAFKA_URL = 'kafka:29092'
+        AGENT_URL = 'http://scrape-agent:4444/wd/hub'
+        SCRAPE_URL = os.environ['SCRAPE_URL']
+    else:
+        raise ValueError('Environment argument is not a supported value. Should be one of "docker", "host"')
+    
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
@@ -15,8 +32,6 @@ if __name__ == '__main__':
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    scrape_url = os.environ['SCRAPE_URL']
-    # scrape_url = 'https://livetiming.tsl-timing.com/231218'
 
     parser = TableParser()
 
@@ -24,11 +39,10 @@ if __name__ == '__main__':
     logger.info('Internal set up done, sleeping to ensure dependency availability')
     time.sleep(5)
 
-    kafka_producer = Producer({'bootstrap.servers': 'kafka:29092'})
-    # kafka_producer = Producer({'bootstrap.servers': 'localhost:9092'})
+    kafka_producer = Producer({'bootstrap.servers': KAFKA_URL})
 
     logger.info('starting scrape loop')
-    with TslInterface(session_url=scrape_url) as tsl_interface:
+    with TslInterface(session_url=SCRAPE_URL, scrape_agent_url=AGENT_URL) as tsl_interface:
         while True:
             logger.info('getting metadata')
             metadata = tsl_interface._get_metadata()
--- tsl_interface/tsl_interface.py
+++ tsl_interface/tsl_interface.py
@@ -3,12 +3,14 @@ import requests, time, datetime, logging
 
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from selenium.common.exceptions import NoSuchElementException
 
 class TslInterface:
-    def __init__(self, session_url:str):
-        self.url = session_url
+    def __init__(self, session_url:str, scrape_agent_url: str):
+        self.session_url = session_url
+        self.scrape_agent_url = scrape_agent_url
         self.logger = logging.getLogger(__name__)
 
     def __enter__(self):
@@ -33,14 +35,14 @@ class TslInterface:
         options.add_argument("--disable-dev-shm-usage") # // overcome limited resource problems
         options.add_argument("--no-sandbox") # // Bypass OS security model
         self.driver = webdriver.Remote(
-        command_executor='http://scrape-agent:4444/wd/hub',
+        command_executor=self.scrape_agent_url,
         options=options
         )
         #maximize the window size
         self.driver.maximize_window()
         self.logger.info('driver initiated')
         #navigate to browserstack.com
-        self.driver.get(url=self.url)
+        self.driver.get(url=self.session_url)
         self.logger.info('driver loaded webpage')
         time.sleep(3)
         try: 
@@ -53,22 +55,58 @@ class TslInterface:
     def _get_metadata(self) -> dict:
         time.sleep(1)
         try:
-            series = self.driver.find_element(by='id', value='seriesName').text
+            series = self.driver.find_element(by=By.XPATH, value='//span[contains(@data-bind, "seriesName")]').text
         except NoSuchElementException:
             self.logger.warn(f'series metadata could not be found')
             raise
 
         try:
-            session = self.driver.find_element(by='id', value='sessionName').text
+            session = self.driver.find_element(by=By.XPATH, value='//span[contains(@data-bind, "sessionName")]').text
         except NoSuchElementException:
             self.logger.warn(f'session metadata could not be found')
             raise
+
+        try:
+            circuit = self.driver.find_element(by=By.XPATH, value='//span[contains(@data-bind, "trackDisplayName")]').text
+        except NoSuchElementException:
+            self.logger.warn(f'circuit metadata could not be found')
+            raise
+
+        try:
+            weather_conditions = self.driver.find_element(by=By.XPATH, value='//span[contains(@data-bind, "weatherConditions")]').text
+        except NoSuchElementException:
+            self.logger.warn(f'circuit metadata could not be found')
+            raise
+
+        try:
+            circuit_conditions = self.driver.find_element(by=By.XPATH, value='//span[contains(@data-bind, "trackConditions")]').text
+        except NoSuchElementException:
+            self.logger.warn(f'circuit metadata could not be found')
+            raise
+
+        try:
+            session_time_remaining = self.driver.find_element(by=By.ID, value='sessionTime').text
+        except NoSuchElementException:
+            self.logger.warn(f'circuit metadata could not be found')
+            raise
+
+        try:
+            track_status = self.driver.find_element(by=By.ID, value='currentflag').text
+        except NoSuchElementException:
+            self.logger.warn(f'circuit metadata could not be found')
+            raise
+        
         
         session_date = datetime.datetime.now().date()
         metadata = {
             'series': series,
             'session': session,
-            'date': session_date
+            'circuit': circuit,
+            'date': session_date,
+            'weather_conditions': weather_conditions,
+            'circuit_conditions': circuit_conditions,
+            'track_status': track_status,
+            'session_time_remaining': session_time_remaining
         }
     
         return metadata
